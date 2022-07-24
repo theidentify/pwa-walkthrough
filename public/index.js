97b827d2bdbcd63e9ac28db01519287b4e154bc1
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2022-07-24 18:22:40 +0700

    random image shown

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const images = ['p1', 'p2', 'p3', 'p4', 'p5'];
+const imgElement = document.querySelector('img');
+
+function randomValueFromArray(array) {
+  let random = Math.floor(Math.random() * array.length);
+  return array[random]
+}
+
+setInterval(function() {
+  let randomChoice = randomValueFromArray(images);
+  imgElement.src = 'images/' + randomChoice + '.jpg';
+}, 2000)
