7835c9aeed2378a4d2bbc9d87655f09b6ad4a43d
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2022-07-25 20:42:22 +0700

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('video-store').then(function (cache) {
+      return cache.addAll([
+        '/index.html',
+        '/index.js',
+        '/style.css',
+        '/images/p1.webp',
+        '/images/p2.webp',
+        '/images/p3.webp',
+        '/images/p4.webp',
+        '/images/p5.webp',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log(e.request.url);
+  e.responseWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
